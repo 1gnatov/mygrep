d6d3647d567d7455243895aff5ff8dc02a49d5b7
1gnatov <ignatov.msu@gmail.com>
2016-02-05 02:16:10 +0300

    ready to code review :)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,30 +10,44 @@ import static java.nio.file.Paths.get;
 
 public class Main {
 
+    // program arguments
+    //"qwerty" file.txt testdir\\secondfile.txt file3.txt filenottext.bin test.html test2.html test3.html test4.html test5.html
+
     public static void main(String[] args) {
+
+        //time it!
+        long startTime = System.nanoTime();
 
         System.out.println(Arrays.toString(args));
         System.out.println(args[0]);
 
         String que = args[0];
 
-        //List listArgs = Arrays.asList(args);
-        //Stream<String> allFiles = listArgs.subList(1, args.length).stream();
         for (int i=1; i<args.length; i++) {
-        String filename = args[i];
+            String filename = args[i];
+            //System.out.println("Searching in: " + filename);
             try {
                 Stream<String> streamFromFiles = Files.lines(Paths.get(filename));
                 //Stream<String> streamFromFiles = Files.lines(Paths.get("testdir\\secondfile.txt"));
-                Collection result = streamFromFiles
+                Collection<String> result = streamFromFiles
                         .parallel()
                         .filter((s) -> s.contains(que))
                         .collect(Collectors.toList());
+                // result can be 0, 1, or >
                 if (!result.isEmpty()) {
-                    System.out.println("File: " + filename + ", string: " + result.toString());
+                    for (String r: result) {
+                        System.out.println("File: " + filename + ", string: " + r);
+                    }
+
                 }
             } catch (IOException e) {
                 System.out.println("No such file: " + e.toString());
             }
         }
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("It takes " + duration/1000000 + " ms");
+
     }
 }
