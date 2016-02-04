5294d1242517b0dc0d12aeccc8257d372088f2e3
1gnatov <ignatov.msu@gmail.com>
2016-02-05 01:14:47 +0300

    checking empty result collection

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,19 +14,27 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Arrays.toString(args));
+        System.out.println(args[0]);
 
-        String que = "qwerty";
-        try {
-            Stream<String> streamFromFiles = Files.lines(Paths.get("file.txt"));
-            //Stream<String> streamFromFiles = Files.lines(Paths.get("testdir\\secondfile.txt"));
-            Collection result = streamFromFiles
-                    .parallel()
-                    .filter((s) -> s.contains(que))
-                    .collect(Collectors.toList());
-            System.out.println(result.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
+        String que = args[0];
+
+        //List listArgs = Arrays.asList(args);
+        //Stream<String> allFiles = listArgs.subList(1, args.length).stream();
+        for (int i=1; i<args.length; i++) {
+        String filename = args[i];
+            try {
+                Stream<String> streamFromFiles = Files.lines(Paths.get(filename));
+                //Stream<String> streamFromFiles = Files.lines(Paths.get("testdir\\secondfile.txt"));
+                Collection result = streamFromFiles
+                        .parallel()
+                        .filter((s) -> s.contains(que))
+                        .collect(Collectors.toList());
+                if (!result.isEmpty()) {
+                    System.out.println("File: " + filename + ", string: " + result.toString());
+                }
+            } catch (IOException e) {
+                System.out.println("No such file: " + e.toString());
+            }
         }
-
     }
 }
